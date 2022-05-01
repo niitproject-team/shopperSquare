3a43b0c7decc4ce1000030f403fa52514fc68a97
shubham sharma <89904564+shubhsharma0@users.noreply.github.com>
2022-05-01 13:19:31 +0530

    Add files via upload

---
+++ src/java/com/DAO/ProductDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.DAO;
+
+import com.entity.Product;
+import java.util.List;
+
+/**
+ *
+ * @author ram
+ */
+public interface ProductDAO {
+    
+    public boolean addproduct (Product pro);
+    
+    public List<Product> getAllproducts();
+    
+    public Product getProductById(int id);
+    
+    public boolean updateEditProduct(Product pro );
+    
+    public boolean deleteProduct(int id);
+    
+}
---
+++ src/java/com/DAO/ProductDAOImpl.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.DAO;
+
+import com.entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProductDAOImpl {
+    
+     private Connection conn;
+     
+     public ProductDAOImpl(Connection conn){
+         super();
+         this.conn = conn;
+     }
+    
+    public boolean addproduct(Product pro){
+        boolean f = false;
+        try{
+            String sql = "insert into product(productname,productdescription,productprice,productquantity,productdiscount,productcategory,productstatus,photo,user_email) value(?,?,?,?,?,?,?,?,?)"; 
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, pro.getProductname());
+            ps.setString(2, pro.getProductdescription());
+            ps.setString(3, pro.getProductprice());
+            ps.setString(4, pro.getProductquantity());
+            ps.setString(5, pro.getProductdiscount());
+            ps.setString(6, pro.getProductcategory());
+            ps.setString(7, pro.getProductstatus());
+            ps.setString(8, pro.getPhotoName());
+            ps.setString(9, pro.getEmail());
+            
+            
+            int i = ps.executeUpdate();
+            
+            if(i==1){
+                f = true;
+            }
+            
+        }catch ( Exception e){
+            e.printStackTrace();
+        }
+        
+        return f;
+    }
+    
+    public List<Product> getAllproducts(){
+        
+        List<Product> list= new ArrayList<Product>();
+        Product pro = null;
+        
+        
+        try{
+            
+            String sql = "select * from product";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next())
+            {
+                pro= new Product();
+            
+                pro.setProductId(rs.getInt(1));
+                pro.setProductname(rs.getString(2));
+                pro.setProductdescription(rs.getString(3));
+                pro.setProductprice(rs.getString(4));
+                pro.setProductquantity(rs.getString(5));
+                pro.setProductdiscount(rs.getString(6));
+                pro.setProductcategory(rs.getString(7));
+                pro.setProductstatus(rs.getString(8));
+                pro.setPhotoName(rs.getString(9));
+                pro.setEmail(rs.getString(10));
+                
+                list.add(pro);
+            }
+            
+            
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+    
+    public Product getProductById(int id){
+        
+        Product pro = null;
+        
+        
+        try{
+            
+            String sql = "select * from  product where productId=? ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            
+            ps.setInt(1, id);
+            
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                
+                pro= new Product();
+                
+                 pro.setProductId(rs.getInt(1));
+                pro.setProductname(rs.getString(2));
+                pro.setProductdescription(rs.getString(3));
+                pro.setProductprice(rs.getString(4));
+                pro.setProductquantity(rs.getString(5));
+                pro.setProductdiscount(rs.getString(6));
+                pro.setProductcategory(rs.getString(7));
+                pro.setProductstatus(rs.getString(8));
+                pro.setPhotoName(rs.getString(9));
+                pro.setEmail(rs.getString(10));
+            }
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return pro;
+    }
+    
+    
+    public boolean updateEditProduct(Product pro ) {
+        boolean f = false;
+        try{
+            String sql = "update product set productname=?,productdescription=?,productprice=?,productquantity=?,productdiscount=?,productstatus=? where productid=? ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+             
+            ps.setString(1, pro.getProductname());
+            ps.setString(2, pro.getProductdescription());
+            ps.setString(3, pro.getProductprice());
+            ps.setString(4, pro.getProductquantity());
+            ps.setString(5, pro.getProductdiscount());
+            ps.setString(6, pro.getProductstatus());
+            ps.setInt(7, pro.getProductId());
+            
+            int i= ps.executeUpdate();
+            if (i==1){
+                f=true;
+            }
+            
+        }catch ( Exception e){
+           e.printStackTrace();
+        }
+        return f;
+    }
+
+    
+      public boolean deleteProduct(int id){
+          boolean f = false;
+          try{
+              String sql = "delete from product where productId=?";
+              PreparedStatement ps = conn.prepareStatement(sql);
+              ps.setInt(1, id);
+              int i = ps.executeUpdate();
+              if(i==1){
+                  
+                  f = true;
+              }
+              
+          }catch (Exception e){
+              e.printStackTrace();
+          }
+          
+          return false;
+      }
+}
